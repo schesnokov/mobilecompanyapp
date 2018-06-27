f4553e80a55423a516b1f25333c21f7e90009e6f
chesnokov <chesnokov.sergei@gmail.com>
2018-06-27 23:03:27 +0300

    Adding @Trasactional
    Add @Transactional annotation

--- src/main/java/com/mobilecompany/dao/impl/OptionDaoImpl.java
+++ src/main/java/com/mobilecompany/dao/impl/OptionDaoImpl.java
@@ -3,6 +3,7 @@ package com.mobilecompany.dao.impl;
 import com.mobilecompany.dao.api.OptionDao;
 import com.mobilecompany.entities.Option;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
@@ -14,43 +15,34 @@ public class OptionDaoImpl implements OptionDao {
     @PersistenceContext
     private EntityManager entityManager;
 
+    @Transactional
     @Override
     public void create(Option option) {
-        //EntityManager entityManager = entityManagerFactory.createEntityManager();
-        entityManager.getTransaction().begin();
         Option option1 = new Option();
         entityManager.persist(option);
-        entityManager.getTransaction().commit();
-        entityManager.close();
     }
 
+    @Transactional
     @Override
     public Option read(Integer a) {
-        //EntityManager entityManager = entityManagerFactory.createEntityManager();
         Option option = entityManager.find(Option.class, a);
-        entityManager.close();
         return option;
     }
 
+    @Transactional
     @Override
     public void update(Option option) {
-        //EntityManager entityManager = entityManagerFactory.createEntityManager();
         Option option1 = entityManager.find(Option.class, 1);
         entityManager.detach(option);
-        entityManager.getTransaction().begin();
         option.setName("NewName");
         option.setPrice(new BigDecimal(20.00));
         option.setConnectionCost(new BigDecimal(10.00));
-        entityManager.close();
     }
 
+    @Transactional
     @Override
     public void delete(Integer id) {
-        //EntityManager entityManager = entityManagerFactory.createEntityManager();
         Option option = entityManager.find(Option.class, 1);
-        entityManager.getTransaction().begin();
         entityManager.remove(option);
-        entityManager.getTransaction().commit();
-        entityManager.close();
-    }
+        }
 }
